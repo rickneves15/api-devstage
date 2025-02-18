d3f18f1743c6967ec7a8283d0adfdd035eb757d8
Richard Neves <ricknevesbc@icloud.com>
2025-02-18 15:03:43 -0300

    feat: adds event subscription functionality
    Adds a new route for subscribing to events.  Implements basic Zod validation for request and response data.  Configures environment variables using Zod.  Sets up Fastify with CORS, Swagger, and Swagger UI.

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+
+const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors)
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Dev Stage',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('HTTP Server Running')
+})
